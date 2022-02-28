3301714c92df95e6766116de7eab8b630c188db5
Vasily <vastracer@gmail.com>
2022-02-28 15:55:45 +0300

    release 1.0

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,18 @@ use std::io::Seek;
 use std::io::SeekFrom;
 use std::io::prelude::*;
 use chrono::prelude::*;
+use std::path::Path;
+use std::fs;
+use std::os::unix::fs::MetadataExt;
+use std::time::SystemTime;
 
-const VER: &str = "0.1";
+const VER: &str = "1.0";
+
+struct Files {
+    name: String,
+    create_date: SystemTime,
+    size: usize
+}
 
 fn write_log_message(log_file: &mut std::fs::File , message: &str) {
     let local_time: DateTime<Local> = Local::now();
@@ -23,6 +33,31 @@ fn write_log_message(log_file: &mut std::fs::File , message: &str) {
     };
 }
 
+fn get_files_list(_dir: &Path, ext: &String, _list: &mut Vec<Files>){
+    if _dir.is_dir() {
+        for entry in fs::read_dir(_dir).expect("Directory traversal error") {
+            let entry = entry.expect("Directory read error");
+            let path = entry.path();
+            if path.is_dir() {
+                get_files_list(&path, &ext, _list);
+            } else {
+                if ext == "" || *ext == path.extension().unwrap().to_os_string().into_string().unwrap() {
+                    let p: String = path.into_os_string().into_string().unwrap();
+                    let md = std::fs::metadata(Path::new(&p)).unwrap();
+                    let t: SystemTime = md.created().unwrap();
+
+                    let fl = Files {
+                        name: p,
+                        create_date: t,
+                        size: md.size() as usize,
+                    };
+                    _list.push(fl);
+                }
+            }
+        }
+    }
+}
+
 
 fn main() {
     let mut args: Vec<String> = env::args().collect();
@@ -32,6 +67,7 @@ fn main() {
     let mut ext: String = String::from("");
     let mut size: usize = 0;
     let mut log_file: Option<std::fs::File> = None;
+    let mut dry_run: bool = false;
 
     println!("RemoveOLD v{}", VER);
     args.remove(0); // remove programm name
@@ -91,13 +127,17 @@ fn main() {
                 log_write_flag = true;
             },
             "help" | "-?" | "-help" => {
-                // TODO: show help and validate it
+                // show help
                 println!("Command line arguments:");
                 println!("  help - show this help");
                 println!("  size=number - if number is greater then free space, the program will search for and deleting the oldest files. This parameter is required.");
                 println!("  ext=extension - if specify this parameter, the application will only look for files with that extension.");
                 println!("  log=log_file_name - if specify this parameter, the application will save the names of deleted files to this log file.");
+                println!("  dry - process without real files deleting");
                 return;
+            },
+            "dry" => {
+                dry_run = true;
             },
             _ => {
                 println!("Invalid parameter: {}",t_arg[0]);
@@ -111,7 +151,7 @@ fn main() {
         return;
     }
 
-    // TODO: open log file
+    // open log file
     if log_write_flag {
         log_file = match OpenOptions::new().read(true).write(true).create(true).open(log.clone()){
             Ok(l) => Some(l),
@@ -133,8 +173,9 @@ fn main() {
         log_file = Some(t);
     }
 
-    // TODO: analyze space size
-    let cwd = CString::new(env::current_dir().unwrap().as_os_str().as_bytes()).unwrap();
+    // analyze space size
+    let cur_dir = env::current_dir().unwrap();
+    let cwd = CString::new(cur_dir.as_os_str().as_bytes()).unwrap();
     let free_space: usize;
     unsafe {
         let mut stat_buf: libc::statvfs = mem::zeroed();
@@ -143,17 +184,55 @@ fn main() {
     }
     println!("free space: {} bytes", free_space);
 
-    // TODO: find files to delete
     if free_space < size {
+        let mut list: Vec<Files> = Vec::new();
+    // seek files to delete
+    get_files_list(&cur_dir, &ext, &mut list);
+
+        // sort and find slice of files to remove
+        list.sort_by(|a,b| a.create_date.cmp(&b.create_date));
+        let mut sum: usize = 0;
+        let mut counter: u32 = 0;
+        let files_number = list.len();
+        if dry_run {
+            let mut t = log_file.expect("Something goes wrong!");
+            write_log_message(&mut t, "DRY mode");
+            log_file = Some(t);
+        }
+        for item in list {
+            //let _datetime: DateTime<Utc> = item.create_date.into();
+            let res: Result<(), std::io::Error>;
+            if dry_run {
+                res = Ok(());
+            } else {
+                res = std::fs::remove_file(item.name.clone());
+            }
+            match res {
+                Ok(_r) => {
+                    sum += item.size;
+                    counter += 1;
+
+                    let mut t = log_file.expect("Something goes wrong!");
+                    write_log_message(&mut t, format!("Cleared {} bytes by deleting {} ", item.size, item.name ).as_str());
+                    log_file = Some(t);
+
+                    if sum > size - free_space {
+                        break;
+                    }
+                },
+                Err(e) => {
+                    println!("Something goes wrong during deleting file. {:?}", e);
+                }
+            }
+
+        }
+        println!("Checked {} files. deleted {} files. size of deleted files {} bytes.",files_number, counter, sum);
 
     }
-    // TODO: finish
+    // finish
 
     if log_write_flag {
         let mut t = log_file.expect("Something goes wrong!");
         write_log_message(&mut t, "Application finished");
     }
-    // TODO: remove those lines
-    println!("ext: {}", ext);
-    println!("log: {}", log);
 }
